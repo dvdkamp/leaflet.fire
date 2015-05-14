51b2a4c53567c2b4b2c40f43341c7d1eccc9ceb9
Derek van der Kamp <derek.vanderkamp@gmail.com>
2015-05-14 10:45:54 -0700

    Tidied up the scripts a bit

--- server.R
+++ server.R
@@ -1,23 +1,27 @@
 # server.R
 library(rgdal)
 library(leaflet)
+library(dplyr)
 
 shinyServer(function(input, output) {
 
+  ## Read in fire perimeter data
+  fire.perims = subset(readOGR("data/","fire.perims.ll.large"),FIRE_YEAR>=1950)
   
-  fire.perims = subset(readOGR("data/","fire.perims.ll.large"),FIRE_YEAR>=1960)
-  fire.counts = as.data.frame(table(fire.perims@data$FIRE_YEAR))
-  fire.counts$Var1 = as.numeric(as.character(fire.counts$Var1))
+  ## create yearly fire count dataset
+  fire.counts = fire.perims@data %>% group_by(FIRE_YEAR) %>% summarise(fire.count = n())
      
+  ## create bar graph where the colour is dictated by the selected year
   output$bar.graph <-renderPlot({
       par(mar = c(2.2,2,0.3,0))
-      barplot(fire.counts$Freq,
-              names = fire.counts$Var1,
-              col = c("red","blue")[(as.numeric(fire.counts$Va==input$slider1) +1)],
+      barplot(fire.counts$fire.count,
+              names = fire.counts$FIRE_YEAR,
+              col = c("red","blue")[(as.numeric(fire.counts$FIRE_YEAR==input$slider1) +1)],
               ylab = "")
-    title("Fire Counts", line = -2,cex.main = 2)
+    title("Yearly Fire Counts", line = -2,cex.main = 2)
   })  
-  
+ 
+  ## generate leaflet map centred over BC 
   output$leaflet.map <- renderLeaflet({
     leaflet() %>% 
       setView(-123.976, 53.965, zoom = 5) %>% addTiles() %>% addPolygons(data = subset(fire.perims,FIRE_YEAR == input$slider1))
--- ui.R
+++ ui.R
@@ -3,11 +3,11 @@ library(leaflet)
 
 shinyUI(fluidPage(
   
-       sliderInput("slider1", label = h4("Historical wildfires in British Columbia (> 200 hectres)"),
-                  sep = "",width = 750,
-                  min = 1960, max = 2013, value = 2013),
+       sliderInput("slider1", label = h4("Historical wildfires in British Columbia (> 400 hectares)"),
+                  sep = "",width = 730,
+                  min = 1950, max = 2013, value = 2013),
  
-         plotOutput('bar.graph',height = 160,width = 750),
-         leafletOutput('leaflet.map',height = 520,750)
+         plotOutput('bar.graph',height = 160,width = 730),
+         leafletOutput('leaflet.map',height = 520,width = 730)
   
   ))
